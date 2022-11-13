8c0959389db99a38481a630976709e850f7ac1a9
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-11-13 16:46:04 +0900

    wip

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,31 @@
-require('dotenv').config();
+require("dotenv").config();
+
+const { createStore } = require("./utils");
+
+const LaunchAPI = require("./datasources/launch");
+const UserAPI = require("./datasources/user");
+
+const store = createStore();
+const resolvers = require("./resolvers");
+
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store }),
+  }),
+});
+
+const port = 4002;
+
+server.listen({ port: port }).then(() => {
+  console.log(`
+    Server is running!
+    Listening on port ${port}
+    Explore at https://studio.apollographql.com/sandbox
+  `);
+});
